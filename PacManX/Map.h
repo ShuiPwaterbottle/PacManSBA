e364bdcbbeed0146c50d358c1387587d84cef431
Lenovo <Lenovo@LAPTOP-J2JRLV0I>
2023-11-21 22:04:05 +0800

    添加项目文件。

---
+++ PacManX/Map.h
@@ -0,0 +1,65 @@
+#pragma once
+#include"Position.h"
+#include<vector>
+#include<queue>
+using namespace std;
+
+const int MAP_SIZE = 29;
+const int PACMAN_X = MAP_SIZE / 2;
+const int PACMAN_Y = MAP_SIZE / 2;
+
+//时钟周期
+extern const int TIME_SPEED;
+const int MAX_FREEZE_TIME = 5 * 1000 / TIME_SPEED;
+
+//Type of the point on the map, such as these:
+enum MAP_POS_TYPE { SPACE,WALL,PEAN,SUPER_PEAN };
+class MapPos: public Position
+{
+	int type; 
+public:
+	//Set the type of that point
+	void setType(int t);
+	int getType() { return type; }
+};
+
+//用于寻路的坐标结构体
+struct PosXY
+{
+	int x, y;
+};
+
+class Pacman;
+class Ghost;
+class Map
+{
+	//Every point of the map, seen as different types of elements.
+	MapPos points[MAP_SIZE][MAP_SIZE];
+	//Player's score
+	int scores;
+	//Winning score
+	int target_scores;
+	//The freezing time of the ghosts after eating the super pean
+	int freezeTime;
+	void findPath(Position &A, Position&B);
+
+public:
+	friend class Game;
+	friend class Ghost;
+	Map() {};
+	//initialize map using file
+	void init(const char* filepath, Pacman &pacman, vector<Ghost> &ghosts);
+	//print the point on (x,y)
+	void renew(int x, int y) { points[x][y].print(); }
+	//return the type of point on (x,y)
+	int goXY(int x, int y) { return points[x][y].getType(); }
+	//delete the pean eaten by the player on (x,y)
+	void delPean(int x, int y);
+	//check (x,y) is exceed from the map or not
+	bool oK(int x, int y);
+	//delete the pean eaten by the player on (x,y)
+	void delSuperPean(int x, int y);
+	//find path
+	int findDir(Position &A, Position&B);
+};
+
---
+++ PacManX/Position.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include<string>
+#include<cstring>
+#include<iostream>
+#include<fstream>
+using namespace std;
+#include"commonh.h"
+
+//…œœ¬◊Û”“
+enum DIRECTION { UP, DOWN, LEFT, RIGHT };
+
+class Position
+{
+protected:
+	string key;//The string of that point
+	int x, y;  //The coordinate of that point
+	int color; 
+public:
+	friend class Map;
+	Position() {
+		color = BLUE_GREEN_COLOR;
+	}
+	Position(int a, int b, const string str,int icolor = BLUE_GREEN_COLOR) {
+		setXY(a, b);
+		color = icolor;
+		setKey(str);
+	}
+	//Set the string
+	void setKey(const string str);
+	//Set the coordinate
+	void setXY(int a, int b);
+	//Print the string on particular coordinate
+	void print();
+	//Clear the string on particular coordinate
+	void clear();
+};
+
